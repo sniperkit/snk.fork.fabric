47ea13e5f4d2eb7173a0fc931323c8e07d06cb37
Jeremy Khawaja <jeremy.khawaja@yahoo.com>
2017-05-12 22:04:07 -0500

    adding access types and procedures for acting on dependeny graph nodes

---
+++ access_types.go
@@ -0,0 +1,9 @@
+package fabric
+
+type AccessType struct {
+}
+
+// NOTE: good article on function types: http://jordanorelli.com/post/42369331748/function-types-in-go-golang
+
+// TODO: determine if this is a complete function signature for an AccessProcedure
+type AccessProcedure func(DGNode)
--- examples/list.go
+++ examples/list.go
@@ -2,7 +2,11 @@ package main
 
 // NOTE: this is based on a Go package found here: https://golang.org/src/container/list/list.go
 
-import "github.com/JKhawaja/fabric"
+import (
+	"log"
+
+	"github.com/JKhawaja/fabric"
+)
 
 type element struct {
 	next, prev *Element
@@ -22,22 +26,36 @@ func (e *elementNode) ID() int {
 
 // list satisfies fabric CDS interface
 type list struct {
-	root element
-	len  int
+	Root element
+	Len  int
 }
 
 func NewList() *list {
-
+	return &list{}
 }
 
-func (l *list) ListNodes() fabric.NodeList {
-
+func (l *list) ListNodes() (fabric.NodeList, error) {
+	// TODO: traverse list and wrap each element as an elementNode, and return elementNode slice
 }
 
-func (l *list) ListEdges() fabric.EdgesMap {
-
+func (l *list) ListEdges(nodes fabric.NodeList) (fabric.EdgesMap, error) {
+	// TODO: traverse NodeList add each Node as a Key, and its next and previous elements in []int slice
 }
 
 func main() {
+
+	var myList list
+
+	nodes, err := myList.ListNodes()
+	if err != nil {
+		log.Printf("Error while traversing CDS and creating node objects: ", err)
+	}
+
+	edges, err := myList.ListEdges(nodes)
+	if err != nil {
+		log.Printf("Error while adding edges to Edges Map: ", err)
+	}
+
+	// TODO: now we can add nodes and edges as needed to a UI object
 
 }
