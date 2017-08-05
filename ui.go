4ea20028570270dc10f32ca52907df3b581629c2
Jeremy Khawaja <jeremy.khawaja@yahoo.com>
2017-08-04 22:48:53 -0500

    adding EmptyUI struct type that can be used when a fabricated CDS system will not be using spatial virtualization

--- ui.go
+++ ui.go
@@ -18,6 +18,88 @@ type UI interface {
 	IsVirtual() bool // specifies whether a UI is virtual or not
 }
 
+// EmptyUI can be used when a UI is needed codewise, but the CDS system will not be using
+// any spatial virtualization (UI DDAGs).
+type EmptyUI struct {
+	AccessProcedures *ProcedureList
+	Signalers        *SignalingMap
+	Signals          *SignalsMap
+	CDS              Section
+}
+
+// NewEmptyUI ...
+func NewEmptyUI() UI {
+	sm := make(SignalingMap, 0)
+	s := make(SignalsMap, 0)
+	p := make(ProcedureList, 0)
+
+	return &EmptyUI{
+		Signalers:        &sm,
+		Signals:          &s,
+		AccessProcedures: &p,
+	}
+}
+
+// ID ...
+func (u *EmptyUI) ID() int {
+	return 0
+}
+
+// GetType ...
+func (u *EmptyUI) GetType() NodeType {
+	return UINode
+}
+
+// GetPriority ...
+func (u *EmptyUI) GetPriority() int {
+	return 0
+}
+
+// ListProcedures ...
+func (u *EmptyUI) ListProcedures() ProcedureList {
+	return *u.AccessProcedures
+}
+
+// ListSignals ...
+func (u *EmptyUI) ListSignals() SignalsMap {
+	return *u.Signals
+}
+
+// ListSignalers ...
+func (u *EmptyUI) ListSignalers() SignalingMap {
+	return *u.Signalers
+}
+
+// UpdateSignaling ...
+func (u *EmptyUI) UpdateSignaling(sm SignalingMap, s SignalsMap) {
+	*u.Signalers = sm
+	*u.Signals = s
+}
+
+// Signal ...
+func (u *EmptyUI) Signal(s NodeSignal) {
+	sm := *u.Signalers
+
+	for _, c := range sm {
+		c <- s
+	}
+}
+
+// GetSection ...
+func (u *EmptyUI) GetSection() Section {
+	return u.CDS
+}
+
+// IsUnique ...
+func (u *EmptyUI) IsUnique() bool {
+	return false
+}
+
+// IsVirtual ...
+func (u *EmptyUI) IsVirtual() bool {
+	return false
+}
+
 // NOTE: VUIs can be part of VUI Dependency Graphs
 //	but each VUI *must* have a lifespan shorter than its dependents.
 //	A VUI can have both real and virtual dependents and it can
